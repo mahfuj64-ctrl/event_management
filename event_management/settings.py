243f14f13c328bee749f8b237fdc74c1070a78b5
Mahfuj <bmmahfuj70264@gmail.com>
2025-07-25 04:46:34 +0600

    Final settings.py for render deployment

--- event_management/settings.py
+++ event_management/settings.py
@@ -1,143 +1,10 @@
-
-# from pathlib import Path
-# import os
-
-# # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# # Quick-start development settings - unsuitable for production
-# # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# # SECURITY WARNING: keep the secret key used in production secret!
-# # SECRET_KEY = 'django-insecure-r(muji1h147vj)b1jaatk!)d_*3u=gvtu9m$_&o2zn-n&r#-s3'
-# SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-# # DEBUG = True
-
-# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-
-
-# # Application definition
-
-# INSTALLED_APPS = [
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',
-#     'events',
-#     'users',
-#     'tailwind',
-#     'django_browser_reload',
-#     'debug_toolbar',
-# ]
-
-# # TAILWIND_APP_NAME = 'theme'
-# # INTERNAL_IPS = ['127.0.0.1']
-
-# MIDDLEWARE = [
-        
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-#     'debug_toolbar.middleware.DebugToolbarMiddleware',
-#     'django_browser_reload.middleware.BrowserReloadMiddleware',
-# ]
-
-# ROOT_URLCONF = 'event_management.urls'
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [BASE_DIR / 'templates'],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-
-# WSGI_APPLICATION = 'event_management.wsgi.application'
-
-
-# # Database
-# # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-# # Password validation
-# # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
-
-
-# # Internationalization
-# # https://docs.djangoproject.com/en/5.2/topics/i18n/
-
-# LANGUAGE_CODE = 'en-us'
-
-# TIME_ZONE = 'UTC'
-
-# USE_I18N = True
-
-# USE_TZ = True
-
-
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-# STATIC_URL = 'static/'
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
-
-# # Default primary key field type
-# # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
-
-import os
 from pathlib import Path
+import os
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# # SECURITY WARNING: don't run with debug turned on in production!
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 
@@ -145,6 +12,8 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
+
+# # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -154,12 +23,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # তোমার অ্যাপগুলো
     'events',
     'users',
 ]
 
-# Development only apps — debug_toolbar, tailwind, browser_reload
+
 if DEBUG:
     INSTALLED_APPS += [
         'tailwind',
@@ -167,8 +35,15 @@ if DEBUG:
         'debug_toolbar',
     ]
 
+# # TAILWIND_APP_NAME = 'theme'
+# # INTERNAL_IPS = ['127.0.0.1']
+
+if DEBUG:
+    INTERNAL_IPS = ['127.0.0.1']
+
 
 MIDDLEWARE = [
+        
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -205,6 +80,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'event_management.wsgi.application'
 
 
+# # Database
+# # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -212,6 +90,9 @@ DATABASES = {
     }
 }
 
+
+# # Password validation
+# # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -229,6 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# # Internationalization
+# # https://docs.djangoproject.com/en/5.2/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -238,14 +122,21 @@ USE_I18N = True
 USE_TZ = True
 
 
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static'
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
